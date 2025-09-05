ccc7219f47c54caefc481e081209ee24e0940e2c
MD SOHAG MIYA <83050584+dev-mdsohagmiya@users.noreply.github.com>
2025-09-05 22:29:14 +0600

    updated auth action base url

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -2,7 +2,7 @@
 
 import { redirect } from "next/navigation";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function registerUser(formData) {
   try {
